add0189043ee62965930eec5384261bc5214c56c
tusharkumar07 <tusharkumar0510@gmail.com>
2024-02-15 19:37:25 +0530

    Backend connected with hosted frontend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors(
   {
-    origin: ["http://localhost:3000"],
+    origin: ["https://task-manager-livid-kappa.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
   }
